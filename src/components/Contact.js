975316859bcfbb1e374a8d62e7dac24c2f00446c
Caleb Redd <caleb.redd@yahoo.com>
2019-10-03 13:06:57 -0700

    Netlify form syntax added

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,7 +9,7 @@ export default function Contact() {
         <h2 className="center">Contact Me</h2>
         {icons}
       </section><div className="main-content">
-        <form action="post">
+        <form netlify name="contact">
           First Name: <input type="text" />
           <br />
           Last Name: <input type="text" />
